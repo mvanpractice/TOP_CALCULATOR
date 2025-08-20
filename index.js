83c69da4d93242d21a3d8509aed70c7481179d09
mvanpractice <michaelmanggay@gmail.com>
2025-08-20 16:48:50 +0800

    Feeling unworthy today. Just saving

--- index.js
+++ index.js
@@ -3,20 +3,6 @@
  * Variables and DOM queries
  */
 
-/**
- * First level query
- */
-const displayPanel = document.querySelector('.display-panel');
-const operationsPanel = document.querySelector('.operations-panel');
-const keysPanel = document.querySelector('.keys-panel');
-
-/**
- * Second level query
- */
-const actions = operationsPanel.querySelector('.actions');
-const operations = operationsPanel.querySelector('.operations');
-const inputDisplay = displayPanel.querySelector('.input-display');
-
 const operationsMap = Object.freeze({
     add: (firstNumber, SecondNumber) => firstNumber + SecondNumber,
     sub: (firstNumber, SecondNumber) => firstNumber - SecondNumber,
@@ -24,18 +10,94 @@ const operationsMap = Object.freeze({
     div: (firstNumber, SecondNumber) => firstNumber / SecondNumber,
 });
 
+const validNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9 , 0];
+const validOperations = ['add', 'sub', 'mul', 'div'];
+const validActions = ['erase', 'clear', 'decimal-point', '='];
+
+const queryOne = factorySelectorMaker('querySelector');
+const queryAll = factorySelectorMaker('querySelectorAll');
+
+/**
+ * First level query
+ */
+const container = queryOne(document, '.container');
+const displayPanel = queryOne(document, '.display-panel');
+const operationsPanel = queryOne(document, '.operations-panel');
+const keysPanel = queryOne(document, '.keys-panel');
+
+/**
+ * Second level query
+ */
+const inputDisplay = queryOne(displayPanel, '.input-display');
+const actionKeys = queryOne(operationsPanel, '.action-keys');
+const operationKeys = queryOne(operationsPanel, '.operation-keys');
+
 /**
  * Functions
  */
 
-function handleKeysPanelClick(event) {
+/**
+ * My first factory function taught by ChatGPT
+ */
+function factorySelectorMaker(selectorMethod) {
+    return (nodeObject, nodeSelector) => {
+        return nodeObject[selectorMethod](nodeSelector);
+    }
+}
+
+function handleContainerClicks(event) {
     event.stopPropagation();
 
-    // inputDisplay.value += event.target.id;  
+    const btnclicked = event.target;
+
+    // Only button clicks
+    if (btnclicked.closest('button')) {
+        
+        // Only valid buttons
+        if (btnclicked.id && btnclicked.id !== 'input-display') {
+            
+            // Check what button is clicked
+            if (validNumbers.includes(parseInt(btnclicked.id))) {
+                
+                // Number
+                handleNumberClick(btnclicked.id);
+            } else if (validActions.includes(btnclicked.id)) {
+
+                // Action
+                handleActionClick(btnclicked.id);
+            } else if (validOperations.includes(btnclicked.id)) {
+
+                // Operation
+                handleOperationClick(btnclicked.id);
+            }
+        }
+    }
+}
+
+function handleNumberClick(clickId) {
+    const inputValue = inputDisplay.value;
+
+    return !inputValue ? inputDisplay.value = clickId : inputDisplay.value += clickId;
+}
+
+function handleActionClick(clickId) {
+    const inputValue = inputDisplay.value;
+    
+    if (!inputValue) {
+        console.log('empty');
+    }
+}
+
+function handleOperationClick(clickId) {
+    const inputValue = inputDisplay.value;
+
+    // if input is empty, return enter number first
+    // if input has value, check if all are numbers
+    // I can't finish this yet, I feel so so so dumbbbb....
 }
 
 /**
  * Event Listeners
  */
 
-keysPanel.addEventListener('click', handleKeysPanelClick);
+container.addEventListener('click', handleContainerClicks);
